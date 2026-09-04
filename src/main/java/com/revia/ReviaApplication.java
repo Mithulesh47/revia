2683bad9a54748683ad00752bd3190287a4715e5
Mithulesh Ponraj <mithunponraj03@gmail.com>
2026-09-04 16:31:44 +0530

    chore: initialize Spring Boot project

---
+++ src/main/java/com/revia/ReviaApplication.java
@@ -0,0 +1,13 @@
+package com.revia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revia/ReviaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
